7efdf08f3aa44f58d6ece327dd53347bfa81d830
Mingwei <s10195172@connect.np.edu.sg>
2022-01-18 13:33:28 +0800

    Initial Commit

---
+++ ToMatcho/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ToMatcho
+//
+//  Created by Kwong Ming Wei on 14/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // pushhhh // hello
+    
+    }
+
+
+}
+
