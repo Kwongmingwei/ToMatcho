71fdef298812f9c7e5ca602bc4d247895e6b4837
Gallant <gallantteo2002@gmail.com>
2022-02-05 04:16:52 +0800

    Joined teams, add teams view

--- ToMatcho/ViewController/AddTeamViewController.swift
+++ ToMatcho/ViewController/AddTeamViewController.swift
@@ -78,21 +78,42 @@ class AddTeamViewController: UIViewController,UITableViewDelegate,UITableViewDat
         alertController.addAction(cancelAction)
         let continueAction = UIAlertAction(title: "Continue",
                                            style: .default) { [weak alertController] _ in
-                                            guard let textFields = alertController?.textFields else { return }
-                                            
-                                            if let roleName = textFields[0].text,
-                                               let roleQuantity = textFields[1].text {
-                                                print("RoleName: \(roleName)")
-                                                print("RoleQuantiy: \(roleQuantity)")
-                                                self.appDelegate.teamRolesList.append(TeamRoles(roleid: "", rolename: roleName, rolequantity: Int(roleQuantity)!))
-                                                self.viewDidLoad()
-                                                self.dismiss(animated: true, completion: nil)
-                                            }
+            guard let textFields = alertController?.textFields else { return }
             
-    }
+            if let roleName = textFields[0].text,
+               let roleQuantity = textFields[1].text {
+                print("test1")
+                print("RoleName: \(roleName)")
+                print("RoleQuantiy: \(roleQuantity)")
+                if roleName.replacingOccurrences(of: " ", with: "") == "" || roleQuantity.replacingOccurrences(of: " ", with: "") == "" {
+                    let alertView = UIAlertController(title: "Empty fields", message: "Fields cannot be empty", preferredStyle: UIAlertController.Style.alert)
+                    alertView.addAction(UIAlertAction(title: "Return", style: UIAlertAction.Style.default, handler: { _ in
+                    }))
+                    self.present(alertView,animated: false,completion: nil)
+                }
+                else{
+                    let Quantity = Int(roleQuantity)
+                    if Quantity != nil {
+                        print("RoleName: \(roleName)")
+                        print("RoleQuantiy: \(roleQuantity)")
+                        self.appDelegate.teamRolesList.append(TeamRoles(roleid: "", rolename: roleName, rolequantity: Int(roleQuantity)!))
+                        self.viewDidLoad()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                    else {
+                        let alertView = UIAlertController(title: "Role Quantity", message: "Role Quantity must be a numeric number", preferredStyle: UIAlertController.Style.alert)
+                        alertView.addAction(UIAlertAction(title: "Return", style: UIAlertAction.Style.default, handler: { _ in
+                        }))
+                        self.present(alertView,animated: false,completion: nil)
+                    }
+                }
+                
+            }
+
+        }
         alertController.addAction(continueAction)
         
-    }
+}
     
     @IBAction func addTeamBtn(_ sender: Any) {
         print("test1")
@@ -104,49 +125,105 @@ class AddTeamViewController: UIViewController,UITableViewDelegate,UITableViewDat
         }
         
         else{
-            print("test2")
-            let db = Firestore.firestore()
-            let uid = Auth.auth().currentUser?.uid
-            let date = Date()
-            let dateFormatter = DateFormatter()
-            //var count = 0
-            dateFormatter.dateFormat = "dd/MM/yyyy"
-            print(dateFormatter.string(from: date))
-            var ref: DocumentReference? = nil
-            var ref2: DocumentReference? = nil
-            print(dateFormatter.string(from: date))
-            ref = db.collection("teams").addDocument(data: [
-                "teamName": txtTeamName.text!,
-                "teamDescription": txtTeamDescription.text!,
-                "gameID": appDelegate.gameID,
-                "createdBy": String(uid!),
-                "createdDate": dateFormatter.string(from: date) ])
-            print("Team added")
-            print(appDelegate.teamRolesList)
-            for i in appDelegate.teamRolesList {
-                db.collection("roles").addDocument(data: [
-                    "roleName": i.roleName,
-                    "roleQuantity": i.roleQuantity,
-                    "teamID": ref!.documentID])
+            if appDelegate.teamRolesList.count == 0 {
+                let alertView = UIAlertController(title: "Empty roles", message: "Roles cannot be empty", preferredStyle: UIAlertController.Style.alert)
+                alertView.addAction(UIAlertAction(title: "Return", style: UIAlertAction.Style.default, handler: { _ in
+                }))
+                self.present(alertView,animated: false,completion: nil)
+            }
+            else {
+                print("test2")
+                var username:String = ""
+                let alertController = UIAlertController(title: "Add Username",
+                                                            message: nil,
+                                                            preferredStyle: .alert)
+                    
+                    self.present(alertController,
+                                 animated: true)
+                alertController.addTextField { (textField) in
+                    textField.placeholder = "userame"
                 }
-            print("Roles Added")
-            let teamid = ref!.documentID
-            ref2 = db.collection("roles").addDocument(data: [
-                "roleName": "Owner",
-                "roleQuantity": 0,
-                "teamID": teamid])
-            db.collection("teamroles").addDocument(data: [
-                "teamid": teamid,
-                "roleid": String(ref2!.documentID),
-                "userid":String(Auth.auth().currentUser!.uid),
-                "uInGameID":"Owner",
-                "roleName":"Owner",
-                "joinedon":dateFormatter.string(from: date)
-            ])
-            appDelegate.teamRolesList = []
-            self.tabBarController?.selectedIndex = 1
-            
-        }
+                let cancelAction = UIAlertAction(title: "Cancel",
+                                                   style: .default) { [weak alertController] _ in
+                                                    self.viewDidLoad()
+                                                    self.dismiss(animated: true, completion: nil)
+                    
+                }
+                alertController.addAction(cancelAction)
+                let continueAction = UIAlertAction(title: "Continue",
+                                                   style: .default) { [weak alertController] _ in
+                    guard let textFields = alertController?.textFields else { return }
+                    
+                    if let userName = textFields[0].text {
+                        print("test1")
+                        print("userame: \(userName)")
+                        if userName.replacingOccurrences(of: " ", with: "") == "" {
+                            let alertView = UIAlertController(title: "Empty fields", message: "Fields cannot be empty", preferredStyle: UIAlertController.Style.alert)
+                            alertView.addAction(UIAlertAction(title: "Return", style: UIAlertAction.Style.default, handler: { _ in
+                            }))
+                            self.present(alertView,animated: false,completion: nil)
+                        }
+                        else{
+                                print("UserName: \(userName)")
+                                username = userName
+                                self.dismiss(animated: true, completion: nil)
+                            let db = Firestore.firestore()
+                            let uid = Auth.auth().currentUser?.uid
+                            let date = Date()
+                            let dateFormatter = DateFormatter()
+                            //var count = 0
+                            dateFormatter.dateFormat = "dd/MM/yyyy"
+                            print(dateFormatter.string(from: date))
+                            var ref: DocumentReference? = nil
+                            var ref2: DocumentReference? = nil
+                            print(dateFormatter.string(from: date))
+                            ref = db.collection("teams").addDocument(data: [
+                                "teamName": self.txtTeamName.text!,
+                                "teamDescription": self.txtTeamDescription.text!,
+                                "gameID": self.appDelegate.gameID,
+                                "createdBy": String(uid!),
+                                "createdDate": dateFormatter.string(from: date) ])
+                            print("Team added")
+                            print(self.appDelegate.teamRolesList)
+                            for i in self.appDelegate.teamRolesList {
+                                db.collection("roles").addDocument(data: [
+                                    "roleName": i.roleName,
+                                    "roleQuantity": i.roleQuantity,
+                                    "teamID": ref!.documentID])
+                                }
+                            print("Roles Added")
+                            
+                            let teamid = ref!.documentID
+                            ref2 = db.collection("roles").addDocument(data: [
+                                "roleName": "Owner",
+                                "roleQuantity": 0,
+                                "teamID": teamid])
+                            print("roles added to db")
+                            db.collection("teamroles").addDocument(data: [
+                                "teamid": teamid,
+                                "roleid": String(ref2!.documentID),
+                                "userid":String(Auth.auth().currentUser!.uid),
+                                "uInGameID": username,
+                                "roleName":"Owner",
+                                "joinedon":dateFormatter.string(from: date)
+                            ])
+                            print("teamroles added to db")
+                            self.appDelegate.teamRolesList = []
+                            //_ = navigationController?.popViewController(animated: true)
+                            _ = self.navigationController?.popToRootViewController(animated: false)
+                            
+                            //self.dismiss(animated: false, completion: nil)
+                            //self.tabBarController?.selectedIndex = 1
+                        }
+                        
+                    }
+
+                }
+                alertController.addAction(continueAction)
+                
+                
+            }
+            }
         
     }
     
---
+++ ToMatcho/ViewController/JoinedTeamViewController.swift
@@ -0,0 +1,185 @@
+//
+//  JoinTeamViewController.swift
+//  ToMatcho
+//
+//  Created by Ho Jun An on 30/1/22.
+//
+import UIKit
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+import SwiftUI
+
+class JoinedTeamViewController: UITableViewController {
+    
+    var myteamsList:[Teams]=[]
+    var userid = Auth.auth().currentUser?.uid
+    var teamidList = [String]()
+    
+    func TeamExist(teamid:String,list:[Teams])->Int {
+
+            for t in list {
+                let id = t.teamId
+                if (id == teamid) {
+                    return 1
+                }
+            }
+        return 0
+    }
+        
+    func loadData(){
+        myteamsList.removeAll()
+        Firestore.firestore().collection("teamroles").whereField("userid", isEqualTo: userid!).getDocuments() {(querySnapshot,err) in
+            if err == nil{
+                //print("b4 myteams count:"+String(self.myteamsList.count))
+                for document in querySnapshot!.documents{
+                    //print("+1")
+                    let teamid=document.get("teamid") as! String
+                    let rolename = document.get("roleName") as! String
+                    print(String(teamid))
+                    print(String(rolename))
+                    if rolename != "Owner" {
+                        Firestore.firestore().collection("teams").document(teamid).getDocument { [self] (document, error) in
+                            if let document = document, document.exists {
+                                let teamname = document.get("teamName") as! String
+                                let teamdesc = document.get("teamDescription") as! String
+                                let gameid = document.get("gameID") as! String
+                                let createdby = document.get("createdBy") as! String
+                                let createdDate = document.get("createdDate") as! String
+                                print ("added")
+                                let exist = TeamExist(teamid: teamid, list: self.myteamsList)
+                                if (exist==0){
+                                    
+                                    self.myteamsList.append(Teams(teamname: teamname, teamid: teamid, teamdesc: teamdesc, gameid: gameid, createdby: createdby, createddate: createdDate))
+                                }
+                                
+                                print("teamciunt" + String(self.myteamsList.count))
+                                self.tableView.reloadData()
+                                print("reload data1")
+                            } else {
+                                print("Document does not exist")
+                            }
+                        }
+
+                    }
+                }
+                
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadData()
+        self.tableView.reloadData()
+        print("reload data2")
+        
+        
+    }
+    
+    
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("tv count" + String(myteamsList.count))
+        return myteamsList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "joinTeamCell", for: indexPath)
+        let team=myteamsList[indexPath.row]
+        cell.textLabel!.textColor = UIColor.white
+        cell.textLabel!.text = "\(team.teamName)"
+        cell.detailTextLabel!.text = "Created On \(team.createdDate)"
+        cell.detailTextLabel!.textColor = UIColor.white
+        print("Cell")
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc=storyboard?.instantiateViewController(withIdentifier: "TeamDetails") as? MyTeamsDetailViewController{
+            vc.teamid = myteamsList[indexPath.row].teamId
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func deleteIt(teamId:String){
+        Firestore.firestore().collection("teamroles").whereField("teamid", isEqualTo: teamId).getDocuments() {(querySnapshot,err) in
+            if err == nil{
+                for document in querySnapshot!.documents{
+                    print("have docs")
+                    let id=String(document.documentID)
+                    Firestore.firestore().collection("teamroles").document(id).delete() { err in
+                        if let err = err {
+                            print("Error removing document: \(err)")
+                        } else {
+                            print("Document successfully removed!")
+                        }
+                    }
+                }
+            }
+        }
+        
+        Firestore.firestore().collection("roles").whereField("teamID", isEqualTo: teamId).getDocuments() {(querySnapshot,err) in
+            if err == nil{
+                for document in querySnapshot!.documents{
+                    print("have docs")
+                    let id=String(document.documentID)
+                    print(id)
+                    Firestore.firestore().collection("roles").document(id).delete() { err in
+                        if let err = err {
+                            print("Error removing document: \(err)")
+                        } else {
+                            print("Document successfully removed!")
+                        }
+                    }
+                }
+            }
+        }
+        
+        Firestore.firestore().collection("teams").document(teamId).delete() { err in
+            if let err = err {
+                print("Error removing document: \(err)")
+            } else {
+                print("Document successfully removed!")
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle:
+        UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            let teamid=String(myteamsList[indexPath.row].teamId)
+            let teamowner = String(myteamsList[indexPath.row].createdBy)
+            if (teamowner == self.userid){
+                let alertView = UIAlertController(title: "Delete team", message: "Deleting team will delete it forever. Are you sure?", preferredStyle: UIAlertController.Style.alert)
+                alertView.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { _ in
+                }))
+                alertView.addAction(UIAlertAction(title: "Delete", style: UIAlertAction.Style.default, handler: { _ in
+                    self.deleteIt(teamId: teamid)
+                    self.loadData()
+                    self.tableView.reloadData()
+                }))
+                self.present(alertView,animated: false,completion: nil)
+            }
+            else{
+                let alertView = UIAlertController(title: "Error!", message: "Error: "+"You do not have permission to delete this team", preferredStyle: UIAlertController.Style.alert)
+                alertView.addAction(UIAlertAction(title: "Return", style: UIAlertAction.Style.default, handler: { _ in
+                }))
+                self.present(alertView,animated: false,completion: nil)
+            }
+            
+        }
+    }
+}
